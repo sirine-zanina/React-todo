5f73eb132b05b72dbd91ebb7c9cf62a67bd39154
unknown <syr.zanina@gmail.com>
2023-12-25 21:27:11 +0100

    todo router

--- server/src/routes/todos.js
+++ server/src/routes/todos.js
@@ -2,11 +2,9 @@ const express = require("express");
 const router = express.Router();
 const todosController = require("../controllers/todosController");
 
-router
-  .route("/")
-  .get(todosController.getAllTodos())
-  .post(todosController.createTodo())
-  .patch(todosController.updateTodo())
-  .delete(todosController.deleteTodo());
+router.route("/").get(todosController.getAllTodos());
+router.route("/todos/add").post(todosController.createTodo());
+router.route("/todos/edit/:id").patch(todosController.updateTodo());
+router.route("/todos/delete/:id").post(todosController.deleteTodo());
 
 module.exports = router;
